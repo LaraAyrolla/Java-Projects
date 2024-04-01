98b41e1f08f8588ff46eaf9fcb694f3406dbfd4f
Lara Ayrolla <lara.laabreu@gmail.com>
2024-03-31 22:19:16 -0300

    feat: starting up first unit tests project

---
+++ Unit-Tests/Basic-Operations/src/entity/Operations.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class Operations {
+    public double biggestValue(double v1, double v2)
+    {
+        return Math.max(v1, v2);
+    }
+
+    public double smallestValue(double v1, double v2)
+    {
+        return Math.min(v1, v2);
+    }
+
+    //compare two objects
+
+    //compare two objects hash
+
+    public double squareRoot(double v1)
+    {
+        return Math.sqrt(v1);
+    }
+
+    public double divide(double v1, double v2)
+    {
+        return v1 / v2;
+    }
+
+    public double multiply(double v1, double v2)
+    {
+        return v1 * v2;
+    }
+
+    public double accumulate(int limit)
+    {
+        return Math.incrementExact(limit);
+    }
+
+    public double sum(double v1, double v2)
+    {
+        return v1 + v2;
+    }
+}
+
---
+++ Unit-Tests/Basic-Operations/src/tests/OperationsTest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import entity.Operations;
+
+import org.junit.*;
+
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
+
+public class OperationsTest {
+    Operations operations = new Operations();
+
+    @Test
+    public void shouldVerifyBiggestValue()
+    {
+        double biggestValue = operations.biggestValue(16, 9);
+
+        Assert.assertEquals(16, biggestValue, 0);
+        Assert.assertEquals(17, biggestValue, 1);
+        Assert.assertEquals(15, biggestValue, 1);
+    }
+
+    @Test
+    public void shouldVerifySmallestValue()
+    {
+        double smallestValue = operations.smallestValue(16, 9);
+
+        Assert.assertFalse(smallestValue == 16);
+        Assert.assertTrue(smallestValue == 9);
+    }
+
+    //test cases 6 and 7 -> compare objects
+
+    @Test
+    public void shouldVerifySquareRoot()
+    {
+        Assert.assertEquals(
+                9.380,
+                operations.squareRoot(88),
+                0.00083151964686
+        );
+    }
+
+    @Test
+    public void shouldVerifyDivision()
+    {
+        Assert.assertEquals(25, operations.divide(50, 2), 0);
+        Assert.assertEquals(POSITIVE_INFINITY, operations.divide(50, 0), 0);
+        Assert.assertEquals(NaN, operations.divide(0, 0), 0);
+    }
+
+    @Test
+    public void shouldVerifyProduct()
+    {
+        Assert.assertEquals(
+                1.0E12,
+                operations.multiply(1000000, 1000000),
+                0
+        );
+    }
+
+    @Test
+    public void shouldVerifySum()
+    {
+        double sum = operations.sum(10, 10);
+
+        Assert.assertEquals(20, sum, 0);
+        Assert.assertFalse(sum == 21);
+        Assert.assertTrue(sum == 20);
+    }
+}
